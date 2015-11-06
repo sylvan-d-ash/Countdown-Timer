da81e8faeb82e7ef01c3c827b8e638fb886feb78
Sylvan Ash <ash@circle.co.ke>
2015-11-06 10:03:39 +0300

    Initial Commit

---
+++ Simple Alerts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Alerts
+//
+//  Created by Sylvan .D. Ash on 11/6/15.
+//  Copyright © 2015 Daitensai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
